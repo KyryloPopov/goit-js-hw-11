6b12f6e2c59bf4cc1871e022bea4f2122a3470fb
KyryloPopov <deecy.popov@gmail.com>
2023-11-20 08:32:36 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,82 @@
 import Notiflix from 'notiflix';
 import { getImages } from './pixabay-api';
 const form = document.querySelector('.search-form');
-form.addEventListener('submit', renderImages);
+form.addEventListener('submit', loadImages);
 const gallery = document.querySelector('.gallery');
-
-function renderImages(event) {
+const button = document.querySelector('.load-more');
+button.addEventListener('click', loadMore);
+let page = 1;
+let value;
+async function loadImages(event) {
   event.preventDefault();
-  const value = event.target.elements.searchQuery.value;
-  getImages(value);
+  button.style.display = 'none';
+  page = 1;
+  value = event.target.elements.searchQuery.value;
+  const data = await getImages(value, page);
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  let cardsMarkup = renderImages(data.hits);
+  gallery.innerHTML = cardsMarkup;
+  if (data.totalHits - 40 * page >= 0) {
+    button.style.display = 'block';
+  }
+  page++;
+}
+
+function renderImages(hits) {
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" width="264px" height="176px" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes</b>
+                <span>${likes}</span>
+                </p>
+                <p class="info-item">
+                <b>Views</b>
+                <span>${views}</span>
+                </p>
+                <p class="info-item">
+                <b>Comments</b>
+                <span>${comments}</span>
+                </p>
+                <p class="info-item">
+                <b>Downloads</b>
+                <span>${downloads}</span>
+                </p>
+            </div>
+        </div>
+        `;
+      }
+    )
+    .join('');
+}
+
+async function loadMore() {
+  button.style.display = 'none';
+  const data = await getImages(value, page);
+  let cardsMarkup = renderImages(data.hits);
+  gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+  button.style.display = 'block';
+  if (data.hits.length === 0) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    return;
+  }
+  page++;
 }
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -2,26 +2,41 @@ import axios from 'axios';
 const queryBuilder = axios.create({
   baseURL: 'https://pixabay.com/api/',
   timeout: 1000,
-  headers: { 'x-api-key': '40778765-86fa5418ec3c51ba5231bc1ca' },
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
 });
 
 const API_KEY = '40778765-86fa5418ec3c51ba5231bc1ca';
 
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const getImages = async param => {
-  try {
-    const responce = await queryBuilder.get('', {
-      params: {
-        key: API_KEY,
-        q: param,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    });
-    console.log(responce);
-  } catch (error) {
-    console.log(error);
-  }
+// Не зміг
+// export const getImages = async param => {
+//   try {
+//     const responce = await queryBuilder.get('', {
+//       params: {
+//         key: API_KEY,
+//         q: param,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//       headers: {
+//         'Access-Control-Allow-Origin': '*',
+//       },
+//     });
+//     console.log(responce);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+export const getImages = async (param, page) => {
+  const response = await fetch(
+    `${BASE_URL}/?key=${API_KEY}&q=${param}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+  );
+  const cards = await response.json();
+  return cards;
 };
